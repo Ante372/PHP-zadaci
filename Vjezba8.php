dc2ec0c09ce91fa16aac1f7d5daff4991923fa41
Ante372 <57566260+Ante372@users.noreply.github.com>
2022-01-07 12:37:30 +0100

    Add files via upload

---
+++ Vjezba8.php
@@ -0,0 +1,36 @@
+<?php
+echo "<p>Oznaci vozilo: </p>";
+$vozilo=array("Audi", "BMW", "Renault","Citroen");
+
+?>
+<!DOCTYPE HTML>
+<html>
+<body>
+<form action="" id="vozilo_forma" method="POST">
+<label for="Audi">Audi</label>
+<input type="radio" id="Audi" value="Audi">
+<label for="BMW">BMW</label>
+<input type="radio" id="BMW" value="BMW">
+<label for="Renault">Renault</label>
+<input type="radio" id="Renault" value="Renault">
+<label for="Citroen">Citroen</label>
+<input type="radio" id="Citroen" value="Citroen">
+</form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
